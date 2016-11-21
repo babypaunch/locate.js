14c32ed2fa29925fb07d92cb343db00156396355
babypaunch <babypaunch@gmail.com>
2016-11-21 20:46:11 +0900

    수정중
    1. 예제 수정
    2. locate 보완중

--- locate.js
+++ locate.js
@@ -7,12 +7,17 @@
 "use strict";
 
 /*
+* locate(json)
+*
 * locate(url)
 * locate(url, string)
 * locate(url, $form)
-*
-* locate(json)
 * locate(url, json)
+*
+* url.locate()
+* url.locate(string)
+* url.locate($form)
+* url.locate(json)
 */
 var locate = function(){
 	var data = undefined;
@@ -24,7 +29,6 @@ var locate = function(){
 			data = arguments[0];
 			data.log = data.log || "[1-1] locate(data)";
 		}
-		console.log("[data-1]", data);
 	} //end: if(arguments.length === 1){
 
 	if(arguments.length === 2){
@@ -35,32 +39,36 @@ var locate = function(){
 			var question = url.indexOf("?") !== -1 || param.indexOf("?") !== -1;
 			location.href = question ? url + param : url + (param === "" ? "" : "?" + param);
 		}else{
-			if(arguments[1].is("form")){ //form
-				arguments[1].attr({action: url}).submit();
-				console.log(arguments[1]);
-			}else{ //json
+			try{
+				if(arguments[1].is("form")){ //form
+					console.log("[args-form]", arguments[1]);
+					arguments[1].attr({action: url}).submit();
+				}
+			}catch(e){ //json
+				console.log("[args-json]", arguments[1]);
 				data = arguments[1];
+				data.url = arguments[0];
 				data.log = data.log || "[1-2] locate(data)";
 			}
 		}
-		console.log("[data-2]", data);
 	} //end: if(arguments.length === 2){
 
 	if(data !== undefined){
 		var D = {
 			url: ""
 			, param: "" //string or json
-			, form: undefined //form element
+			, form: undefined //form element for ajax submit
 			, type: "post" //get or post
 			, sendType: "text" //text(default), json, file
 			, returnType: "text" //text(default), json, jsonp
 			, headers: {} //set ajax header data
+			, callback: "" //set callback name for jsonp
 			, done: undefined //success
 			, fail: undefined //error
-			, bypass: {} //parameter for callback
+			, loading: undefined //show loading callback
+			, unloading: undefined //hide loading callback
 		};
 		$.extend(true, D, data);
-		console.log("[D]", D);
 
 		/*
 		* 비동기 파일 전송시 반드시 contentType과 processData의 값이 false로 고정되야 함.
@@ -69,10 +77,7 @@ var locate = function(){
 			D.contentType = false;
 			D.processData = false;
 			
-			D.param = new FormData();
-			$.each($("#file")[0].files, function(i, file){
-				D.append("file-" + i, file);
-			});
+			D.param = new FromData(D.form[0]);
 		}else{ //text or json
 			var sendType = {
 				text: "application/x-www-form-urlencoded; charset=utf-8"
@@ -84,10 +89,25 @@ var locate = function(){
 
 		if(D.returnType === "jsonp"){
 			D.type = "get"; //jsonp로 받을 경우 반드시 type을 get으로 해야함.
+			D.jsonpCallback = D.callback; //jsonp일때 return받을 callback 함수명 대입
+
+			/*
+			* param의 유형에 따라 callback 함수명 대입
+			* server side에서 동적 callback 함수명 대입을 위해 사용함.
+			*/
+			if($.type(D.param) === "object"){
+				D.param.jsonpCallback = D.callback;
+			}else{
+				D.param += "&jsonpCallback=" + D.callback;
+			}
 		}
 		D.dataType = D.returnType; //set dataType to text/json/jsonp
 
-		//layer open
+		console.log("[D]", D);
+
+		if(D.loading !== undefined){
+			D.loading();
+		}
 		/*
 		* ajax start
 		*/
@@ -99,12 +119,14 @@ var locate = function(){
 			, contentType: D.contentType
 			, processData: D.processData
 			, success: function(data, status, xhr){
-				//layer close;
-				//defaults.bypass
+				if(D.unloading !== undefined){
+					D.unloading();
+				}
 				return arguments;
 			}, error: function(xhr, status, error){
-				//layer close;
-				//defaults.bypass
+				if(D.unloading !== undefined){
+					D.unloading();
+				}
 				return arguments;
 			}
 		}); //end: $.ajax({
