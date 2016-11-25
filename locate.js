dc495f67822d7b84436fe9201f3556353f988f0c
babypaunch <babypaunch@gmail.com>
2016-11-25 10:55:38 +0900

    upload 관련 수정중

--- locate.js
+++ locate.js
@@ -170,7 +170,7 @@ var locate = function(){
 				return xhr;
 				*/
 				if(D.upload !== undefined){
-					return $.ajaxSettings.xhr().upload.onprogress = function(e){
+					$.ajaxSettings.xhr().upload.onprogress = function(e){
 						D.progress(Math.floor(e.loaded / e.total * 100));
 					};
 				}
